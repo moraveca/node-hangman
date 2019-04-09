0c2dffe143ec0358fe7a0df77aea3269d879e152
arm <adrianmoravec@Adrians-MacBook-Pro.local>
2019-04-09 11:39:11 -0500

    initial

---
+++ letter.js
@@ -0,0 +1,32 @@
+// Contains a constructor, Letter. This constructor should be able to either 
+// display an underlying character or a blank placeholder (such as an underscore), 
+// depending on whether or not the user has guessed the letter. 
+// That means the constructor should define:
+
+// A string value to store the underlying character for the letter
+
+// A boolean value that stores whether that letter has been guessed yet
+
+// A function that returns the underlying character if the letter has been guessed, 
+// or a placeholder (like an underscore) if the letter has not been guessed
+
+// A function that takes a character as an argument and checks it against the 
+// underlying character, updating the stored boolean value to true if it was 
+// guessed correctly
+
+var Letter = function(letter) {
+    this.letter = letter;
+    this.guessed = false;
+    this.displayed = function() {
+        if (this.guessed === true) {
+            return this.letter;
+        } else {
+            return "_";
+        }
+    };
+    this.checks = function() {
+        if (userGuess === this.letter) {
+            this.guessed = true;
+        }
+    }
+}
